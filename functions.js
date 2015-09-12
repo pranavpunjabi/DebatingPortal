246865ae704876ce833ae65c3a7907d3fbf365a2
Abhishek Srikanth <srikanth@purdue.edu>
2015-09-12 18:45:41 -0400

    small corrections made regarding spelling in functions.js

--- functions.js
+++ functions.js
@@ -1,22 +1,31 @@
 
+function collapseAll()
+{
+	document.getElementById("page_about").style.visibility="collapse";
+	document.getElementById("page_login").style.visibility="collapse";
+	document.getElementById("page_contactUs").style.visibility="collapse";
+	document.getElementById("page_home").style.visibility="collapse";
+	document.getElementById("page_debate").style.visibility="collapse";
+	document.getElementById("page_policy").style.visibility="collapse";
+	document.getElementById("page_settings").style.visibility="collapse";
+	document.getElementById("page_notifications").style.visibility="collapse";
+}
+
+
 function pageChanged(elem) {
 
-	document.getElementById("page_about").style.visibility="collapse";
-    	document.getElementById("page_login").style.visibility="collapse";
-    	document.getElementById("page_contactus").style.visibility="collapse";
-    	document.getElementById("page_home").style.visibility="collapse";
-    	document.getElementById("page_debate").style.visibility="collapse";
-    	document.getElementById("page_policy").style.visibility="collapse";
-    	document.getElementById("page_settings").style.visibility="collapse";
-    	document.getElementById("page_notifications").style.visibility="collapse";
-    
+	collapseAll();
+
     if(elem.id == "link_about") {
     	document.getElementById("page_about").style.visibility="visible";
     }
-    else if(elem.id == "link_contactus") {
-    	document.getElementById("page_contactus").style.visibility="visible";
-    }
-    else if(elem.id == "link_policy") {
+	else if(elem.id == "link_contactUs") {
+		document.getElementById("page_contactUs").style.visibility="visible";
+	}
+	else if(elem.id == "link_login") {
+		document.getElementById("page_login").style.visibility="visible";
+	}
+	else if(elem.id == "link_policy") {
     	document.getElementById("page_policy").style.visibility="visible";
     }
     else if(elem.id == "link_settings") {
@@ -31,8 +40,8 @@ function pageChanged(elem) {
 
 
 
-    
 
 
-    
+
+
 }
