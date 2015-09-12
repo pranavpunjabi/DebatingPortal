f7a70791acfdb058e649609e504ce41cc802cba6
Abhishek Srikanth <srikanth@purdue.edu>
2015-09-12 18:53:08 -0400

    Changed style.visibility to display and toggle b/w none and initial

--- functions.js
+++ functions.js
@@ -1,41 +1,41 @@
 
-function collapseAll()
+function hideAll()
 {
-	document.getElementById("page_about").style.visibility="collapse";
-	document.getElementById("page_login").style.visibility="collapse";
-	document.getElementById("page_contactUs").style.visibility="collapse";
-	document.getElementById("page_home").style.visibility="collapse";
-	document.getElementById("page_debate").style.visibility="collapse";
-	document.getElementById("page_policy").style.visibility="collapse";
-	document.getElementById("page_settings").style.visibility="collapse";
-	document.getElementById("page_notifications").style.visibility="collapse";
+	document.getElementById("page_about").style.display="none";
+	document.getElementById("page_login").style.display="none";
+	document.getElementById("page_contactUs").style.display="none";
+	document.getElementById("page_home").style.display="none";
+	document.getElementById("page_debate").style.display="none";
+	document.getElementById("page_policy").style.display="none";
+	document.getElementById("page_settings").style.display="none";
+	document.getElementById("page_notifications").style.display="none";
 }
 
 
 function pageChanged(elem) {
 
-	collapseAll();
+	hideAll();
 
     if(elem.id == "link_about") {
-    	document.getElementById("page_about").style.visibility="visible";
+    	document.getElementById("page_about").style.display="initial";
     }
 	else if(elem.id == "link_contactUs") {
-		document.getElementById("page_contactUs").style.visibility="visible";
+		document.getElementById("page_contactUs").style.display="initial";
 	}
 	else if(elem.id == "link_login") {
-		document.getElementById("page_login").style.visibility="visible";
+		document.getElementById("page_login").style.display="initial";
 	}
 	else if(elem.id == "link_policy") {
-    	document.getElementById("page_policy").style.visibility="visible";
+    	document.getElementById("page_policy").style.display="initial";
     }
     else if(elem.id == "link_settings") {
-    	document.getElementById("page_settings").style.visibility="visible";
+    	document.getElementById("page_settings").style.display="initial";
     }
     else if(elem.id == "link_notifications") {
-    	document.getElementById("page_notifications").style.visibility="visible";
+    	document.getElementById("page_notifications").style.display="initial";
     }
     else if(elem.id == "link_logOut") {
-    	document.getElementById("page_home").style.visibility="visible";
+    	document.getElementById("page_home").style.display="initial";
     }
 
 
